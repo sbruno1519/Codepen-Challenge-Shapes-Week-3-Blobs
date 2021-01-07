bc45627c4e99b56821ef6e203368660aeba4f766
Shane Bruno <72577268+sbruno1519@users.noreply.github.com>
2021-01-07 15:01:36 -0600

    buttons.js
    Added in buttons that change the color of background, bubbles, and lamp color. First attempt at JavaScript. I want to figure out make the variables in CSS transfer over to JavaScript so when I change the scheme in CSS it changes in the JavaScript.

---
+++ buttons.js
@@ -0,0 +1,68 @@
+//Get Buttons
+var scheme1button = document.getElementById("scheme1");
+var scheme2button = document.getElementById("scheme2");
+var scheme3button = document.getElementById("scheme3");
+
+//Get Colors
+/*Scheme1*/
+var backcolor1 = document.documentElement.style.getPropertyValue('--backcolor1');
+var lampcolor1 = document.documentElement.style.getPropertyValue('--lampcolor1');
+var bubblecolor1 = document.documentElement.style.getPropertyValue('--bubblecolor1');
+/*Scheme2*/
+var backcolor2 = document.documentElement.style.getPropertyValue('--backcolor2');
+var lampcolor2 = document.documentElement.style.getPropertyValue('--lampcolor2');
+var bubblecolor2 = document.documentElement.style.getPropertyValue('--bubblecolor2');
+/*Scheme3*/
+var backcolor3 = document.documentElement.style.getPropertyValue('--backcolor3');
+var lampcolor3 = document.documentElement.style.getPropertyValue('--lampcolor3');
+var bubblecolor3 = document.documentElement.style.getPropertyValue('--bubblecolor3');
+
+//Buttons
+//Scheme 1
+scheme1button.onclick = function() {
+  //Change Bubble Color
+  document.getElementById("bubble1").style.background = "red";
+  document.getElementById("bubble2").style.background = "red";
+  document.getElementById("bubble3").style.background = "red";
+  document.getElementById("bubble4").style.background = "red";
+  document.getElementById("bubble5").style.background = "red";
+  
+  //Change: Lamp Glass Color
+  document.getElementById("lampglass").style.borderColor = "transparent transparent orange transparent";
+  //Change: Background Color
+  document.querySelector("body").style.background = "#333399";
+}
+
+//Scheme 2
+scheme2button.onclick = function() {
+  //Change: Bubble Color
+  document.getElementById("bubble1").style.background = "#1b6535";
+  document.getElementById("bubble2").style.background = "#1b6535";
+  document.getElementById("bubble3").style.background = "#1b6535";
+  document.getElementById("bubble4").style.background = "#1b6535";
+  document.getElementById("bubble5").style.background = "#1b6535";
+  //Change: Lamp Glass Color
+  document.getElementById("lampglass").style.borderColor = "transparent transparent #a8c66c transparent";
+  //Change: Background Color
+  document.querySelector("body").style.background = "#e1dd72";
+}
+
+//Scheme 3
+scheme3button.onclick = function() {
+  //Change: Bubble Color
+  document.getElementById("bubble1").style.background = "gold";
+  document.getElementById("bubble2").style.background = "gold";
+  document.getElementById("bubble3").style.background = "gold";
+  document.getElementById("bubble4").style.background = "gold";
+  document.getElementById("bubble5").style.background = "gold";
+  //Change: Lamp Glass Color
+  document.getElementById("lampglass").style.borderColor = "transparent transparent lightblue transparent";
+  //Change: Background Color
+  document.querySelector("body").style.background = "blue";
+}
+
+
+
+
+
+
